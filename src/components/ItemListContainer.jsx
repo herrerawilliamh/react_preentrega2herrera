23131e564ef66a70e10e12280b48dff78dea76f5
William Humberto Herrera Rey <herrerawilliamh@gmail.com>
2023-07-12 11:50:49 -0500

    detalles producto, cantidad de elementos

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 
 const ItemListContainer = () => {
     const [productos, setProductos] = useState([]);
+    const [titulo, setTitulo] = useState("Productos")
     const categoria = useParams().categoria;
     // console.log(categoria);
     useEffect(() => {
@@ -12,14 +13,16 @@ const ItemListContainer = () => {
             .then((res) => {
                 if(categoria){
                   setProductos(res.filter((prod) => prod.categoria === categoria));
+                  setTitulo(categoria);
                 }else{
                   setProductos(res);
+                  setTitulo("Productos");
                 }
             })
     }, [categoria])
     
   return (
-    <ItemList productos={productos}/>
+    <ItemList productos={productos} titulo={titulo}/>
   )
 }
 
